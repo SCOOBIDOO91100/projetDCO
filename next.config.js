cdc6005fa5189703efc9e322a77f983c2656926e
Nabil Hafid Alaoui <nabil.hafidalaoui@gmail.com>
2019-11-26 12:06:17 +0100

    first

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// must restart server whenever you make changes in next.config
+module.exports = {
+  env: {
+    // MONGO_SRV: "mongodb+srv://NabilHafid:NabilHafid@jcvd91jcvd-s94ia.mongodb.net/test?retryWrites=true&w=majority",
+    MONGO_SRV:"mongodb+srv://NabHaf:NabHaf@projetcdo-w3rnq.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET: "NabilHafid",
+    CLOUDINARY_URL: "http://api.cloudinary.com/v1_1/dyzxvu13e/image/upload",
+    STRIPE_SECRET_KEY: "sk_test_1qGIhMHOLrBreotOMgAmU9oi00rwgZ9Rdr"
+  }
+};
